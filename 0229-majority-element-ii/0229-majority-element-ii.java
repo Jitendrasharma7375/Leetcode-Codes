1418c9c105d3af786abf24615d31f30b061ad7fa
Jitendra-Sharma <jitendra.sharma7375@gmail.com>
2024-07-25 22:01:46 +0530

    Time: 3 ms (70.61%), Space: 48 MB (47.06%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -2,17 +2,17 @@ class Solution {
     public List<Integer> majorityElement(int[] nums) {
         int n=nums.length;
         double l=Math.floor(n/3);
-        Set<Integer> list=new HashSet<>();
+        List<Integer> list=new ArrayList<>();
         for(int i=0;i<n;i++){
             int count=0;
-            int val=nums[i];
-            for(int j=i;j<n;j++){
-                if(val==nums[j]) count++;
+           if(list.size()==0 || list.get(0)!=nums[i])
+           {
+            for(int j=0;j<n;j++){
+                if(nums[i]==nums[j])count++;
             }
-            if(count>l)
-            {
-                list.add(val); 
-            }
+            if(count>l)list.add(nums[i]);
+           }
+           if(list.size()==2) break;
         }
         return new ArrayList(list);
     }
